95740b88ec2fee5b0d899f745246b149758872a7
Kirby Campbell <jkirbycampbell@gmail.com>
2018-08-14 19:31:34 +0000

    automatically backed up by learn

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,18 @@
 class User < ActiveRecord::Base
   validates_presence_of :name, :email, :password
 
+<<<<<<< HEAD
 
 end
+=======
+  def change
+    create_table :users do |t|
+      t.string :name
+      t.string :email
+      t.string :password
+    end
+
+  end
+  
+end
+>>>>>>> e2f7aad8e2407dd9e441a3f85b055bfcfd36acfb
